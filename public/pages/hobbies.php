4a49aa377ff011fb287ffc9d8fd9fea17e1e5528
Hugo MATHIEU <guedesite77@gmail.com>
2022-01-17 14:31:26 +0100

    Update hobbies.php

--- public/pages/hobbies.php
+++ public/pages/hobbies.php
@@ -1,21 +1,11 @@
 
 <header id="home" class="ytback">
 
-
      <?php include("public/entete.php")?>
 
       <div class="row banner" >
          <div class="banner-text">
-            <h1 class="responsive-headline" data-trad="entete-titre">I'm Hugo Mathieu.</h1>
-            <h3 data-trad="entete-description">I'm a Marseille based computer science student with a passion for <span>web developement</span>, <span>web design</span> and <span>office developement</span>. Let's <a class="smoothscroll" href="#about">start scrolling</a>
-            and learn more <a class="smoothscroll" href="#about">about me</a>.</h3>
-            <hr />
-            <ul class="social">
-               <li><a href="https://twitter.com/Guedesite" target="_blank"><i class="fab fa-twitter"></i></a></li>
-               <li><a href="https://www.linkedin.com/in/hugo-mathieu-2b01bb215/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
-               <li><a href="#"><i class="fab fa-discord"></i></a></li>
-               <li><a href="https://github.com/guedesite" target="_blank"><i class="fab fa-github"></i></a></li>
-            </ul>
+            <h1 class="responsive-headline" data-trad="entete-titre">My Hobbies</h1>
          </div>
       </div>
 
